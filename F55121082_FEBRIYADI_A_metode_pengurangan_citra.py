1f529de99db8a037438deb1dc1636209cf67d334
F55121082_FEBRIYADI <andika.saputra2402@gmail.com>
2023-03-05 17:39:40 +0800

    push2

--- F55121082_FEBRIYADI_A_metode_pengurangan_citra.py
+++ F55121082_FEBRIYADI_A_metode_pengurangan_citra.py
@@ -1,21 +1,16 @@
 import cv2
 
-# load the two input images
-img1 = cv2.imread('image/pengurangan.jpg')
-img2 = cv2.imread('image/pengurangan1.jpg')
+# load dua buah citra yang akan dikurangi
+img1 = cv2.imread('image/pengurangan1.jpg')
+img2 = cv2.imread('image/pengurangan.jpg')
 
-# resize the images to have the same dimensions
-img1 = cv2.resize(img1, (640, 480))
-img2 = cv2.resize(img2, (640, 480))
-
-# convert the images to grayscale
-gray_img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-gray_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-
-# subtract the two images
-diff_img = cv2.subtract(gray_img1, gray_img2)
-
-# show the resulting image
-cv2.imshow('Difference Image', diff_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# pastikan ukuran kedua citra sama
+if img1.shape != img2.shape:
+    print('Ukuran kedua citra berbeda')
+else:
+    # kurangi citra 2 dari citra 1
+    result = cv2.subtract(img1, img2)
+    # tampilkan citra hasil pengurangan
+    cv2.imshow('Result', result)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
