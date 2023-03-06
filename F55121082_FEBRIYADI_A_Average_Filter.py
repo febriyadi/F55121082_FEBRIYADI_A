5a2464739e7ea80c90827ae564454cb2f5de5225
F55121082_FEBRIYADI <andika.saputra2402@gmail.com>
2023-03-06 14:09:55 +0800

    push2

--- F55121082_FEBRIYADI_A_Average_Filter.py
+++ F55121082_FEBRIYADI_A_Average_Filter.py
@@ -1,19 +1,17 @@
 import cv2
 import numpy as np
 
-# Muat gambar
+
 img = cv2.imread('image/Average_Filter.jpg')
 
-# Tentukan ukuran kernel
-kernel_size = 5
+#definisikan kernel size untuk filter
+kernel_size= (5, 5)
 
-# Buat kernelnya
-kernel = np.ones((kernel_size,kernel_size),np.float32)/(kernel_size*kernel_size)
+kernel = np.ones(kernel_size, np.float32) / (kernel_size[0]*kernel_size[1])
 
-# Terapkan filter
-filtered_img = cv2.filter2D(img,-1,kernel)
+filtered_img= cv2.filter2D(img, -1, kernel)
+cv2.imshow('Original car', img)
+cv2.imshow('Filtered car', filtered_img)
 
-# Tampilkan hasilnya
-cv2.imshow('Filtered Image', filtered_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
